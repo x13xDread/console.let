b30a5eef27f26acea6fba141506863d2d5f19953
Miles Weber <vorvon4@gmail.com>
2022-09-26 12:38:52 -0400

    first commit

---
+++ let.js
@@ -0,0 +1,11 @@
+/**
+ * console.let() is a roundabout way of assigning a variable a value using a method.
+ *  
+ * @param {*} value the value you wish to return
+ * @returns value passed in at parameters
+ * 
+ */
+
+console.let = function(value){
+    return value
+}
